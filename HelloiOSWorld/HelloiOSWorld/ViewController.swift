e80f170f69887793e40f0ec74e35c830fd06284b
Steven Gaudin-Lawson <steven.gaudinlawson@gmail.com>
2022-01-17 14:12:26 -0800

    End of Jan 10 class

---
+++ HelloiOSWorld/HelloiOSWorld/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  HelloiOSWorld
+//
+//  Created by user209696 on 1/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Custom Outlets
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    // MARK: View Events
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }//End viewDidLoad
+
+    // MARK: Custom Actions
+    @IBAction func changeGreeting(_ sender: UIButton) {
+        
+        
+    }//End changeGreeting
+    
+}//End ViewController
+
+
+
