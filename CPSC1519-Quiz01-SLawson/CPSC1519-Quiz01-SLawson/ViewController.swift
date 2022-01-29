de0a74c8a5ad10c2ecf3324944c7a5643d49be65
Steven Gaudin-Lawson <steven.gaudinlawson@gmail.com>
2022-01-28 16:55:47 -0800

    Finished product
    Uploading completed Quiz 1 app

--- CPSC1519-Quiz01-SLawson/CPSC1519-Quiz01-SLawson/ViewController.swift
+++ CPSC1519-Quiz01-SLawson/CPSC1519-Quiz01-SLawson/ViewController.swift
@@ -8,13 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Custom Outlets
+    @IBOutlet weak var headerLabel: UILabel!
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var numberButton: UIButton!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // MARK: View Events
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+    }//End viewDidLoad()
+    
+    // MARK: Custom Actions
+    @IBAction func dismissKeyboardTap(_ sender: Any) {
+        numberTextField.resignFirstResponder()
+    }//End dismissKeyboardTap
+    
+    
+    @IBAction func displayEvenOrOdd(_ sender: UIButton) {
+        let number  = Int(numberTextField.text!) ?? 0
+            if number % 2 == 0 {
+                let message = "EVEN number \(number)"
+                resultLabel.text = message
+            } else {
+                let message = "ODD number \(number)"
+                resultLabel.text = message
+            }
+        
+    }//End displayEvenOrOdd
+    
 
 
-}
+}//End UIViewController
 
 
